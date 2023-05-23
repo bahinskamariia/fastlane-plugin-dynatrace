67a727d938ef0458caa6b29b7cecdd2ed77b6081
Mariia Bahinska <mariia.bahinska@kingfisher.com>
2023-05-23 12:08:52 +0300

    Make DTXLogLevel OFF

--- lib/fastlane/plugin/dynatrace/actions/dynatrace_action.rb
+++ lib/fastlane/plugin/dynatrace/actions/dynatrace_action.rb
@@ -76,6 +76,7 @@ module Fastlane
         command << "version=\"#{params[:version]}\""
         command << "symbolsfile=\"#{params[:symbolsfile]}\""
         command << "server=\"#{Helper::DynatraceHelper.without_trailing_slash(params[:server])}\""
+        command << "DTXLogLevel=OFF"
         command << "forced=1" # if the file already exists
         command << "tempdir=\"#{params[:tempdir]}\"" if params[:tempdir]
 
